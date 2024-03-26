6f5c009c462ba450170bba61d9a471e4f606dc9f
sumanron2001 <149249070+sumanron2001@users.noreply.github.com>
2024-03-26 11:21:44 +0530

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(0);
+$al=mysqli_connect("localhost","root","","tours_techvegan");
+/*
+Thanks for Downloading this Project.
+This Project is Designed by Ashish Vegan | https://www.ashishvegan.com
+This Source Code is not for Commercial/Selling Purpose!!
+You can use this source code to Submit Project in College/University
+
+If you want Code Modifications or Customize Project: Contact WhatsApp +919890345539
+
+If you found this Project Helpful Kindly Do Subscribe!!!!
+
+DONATE!
+Google Pay/Phone Pe : 9890345539
+Paypal: ashishvegan@gmail.com
+*/
+?>
